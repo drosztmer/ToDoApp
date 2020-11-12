e8c497f19e6b1305079d627be85ef873c465170c
Drosztmér István <drosztmer.istvan@gmail.com>
2020-11-12 15:59:17 +0100

    Add menu for addfragment

--- app/src/main/java/com/codecool/todoapp/fragments/add/AddFragment.kt
+++ app/src/main/java/com/codecool/todoapp/fragments/add/AddFragment.kt
@@ -1,10 +1,8 @@
 package com.codecool.todoapp.fragments.add
 
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import com.codecool.todoapp.R
 
 class AddFragment : Fragment() {
@@ -13,8 +11,11 @@ class AddFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
+        setHasOptionsMenu(true)
         return inflater.inflate(R.layout.fragment_add, container, false)
     }
 
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.add_fragment_menu, menu)
+    }
 }
