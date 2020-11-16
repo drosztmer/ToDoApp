3679a1dea872b272cc309a93d079ee89cb3d3503
Drosztmér István <drosztmer.istvan@gmail.com>
2020-11-16 10:55:00 +0100

    Create viewmodel

---
+++ app/src/main/java/com/codecool/todoapp/data/viewmodel/ToDoViewModel.kt
@@ -0,0 +1,31 @@
+package com.codecool.todoapp.data.viewmodel
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.viewModelScope
+import com.codecool.todoapp.data.ToDoDatabase
+import com.codecool.todoapp.data.models.ToDoData
+import com.codecool.todoapp.data.repository.ToDoRepository
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
+class ToDoViewModel(application: Application): AndroidViewModel(application) {
+
+    private val toDoDao = ToDoDatabase.getDatabase(application).toDoDao()
+    private val repository: ToDoRepository
+
+    val getAllData: LiveData<List<ToDoData>>
+
+    init {
+        repository = ToDoRepository(toDoDao)
+        getAllData = repository.getAllData
+    }
+
+    fun insertData(toDoData: ToDoData) {
+        viewModelScope.launch(Dispatchers.IO) {
+            repository.insertData(toDoData)
+        }
+    }
+
+}
