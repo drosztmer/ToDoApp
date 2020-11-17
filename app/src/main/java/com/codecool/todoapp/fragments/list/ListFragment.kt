78dc9bc6ccfd0e39e60b3b8b6925b0c061a3574c
Drosztmér István <drosztmer.istvan@gmail.com>
2020-11-17 16:33:11 +0100

    Pass data to updatefragment with safeargs

--- app/src/main/java/com/codecool/todoapp/fragments/list/ListFragment.kt
+++ app/src/main/java/com/codecool/todoapp/fragments/list/ListFragment.kt
@@ -3,11 +3,19 @@ package com.codecool.todoapp.fragments.list
 import android.os.Bundle
 import android.view.*
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.codecool.todoapp.R
+import com.codecool.todoapp.data.viewmodel.ToDoViewModel
 import kotlinx.android.synthetic.main.fragment_list.view.*
 
 class ListFragment : Fragment() {
+
+    private val mTodoViewModel: ToDoViewModel by viewModels()
+
+    private val listAdapter: ListAdapter by lazy { ListAdapter() }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -15,12 +23,18 @@ class ListFragment : Fragment() {
     ): View? {
         val view =  inflater.inflate(R.layout.fragment_list, container, false)
 
-        view.floatingActionButton.setOnClickListener {
-            findNavController().navigate(R.id.action_listFragment_to_addFragment)
+        val recyclerView = view.recycler_view
+        recyclerView.apply {
+            adapter = listAdapter
+            layoutManager = LinearLayoutManager(requireActivity())
         }
 
-        view.list_layout.setOnClickListener {
-            findNavController().navigate(R.id.action_listFragment_to_updateFragment)
+        mTodoViewModel.getAllData.observe(viewLifecycleOwner, Observer { data ->
+            listAdapter.setData(data)
+        })
+
+        view.floatingActionButton.setOnClickListener {
+            findNavController().navigate(R.id.action_listFragment_to_addFragment)
         }
 
         setHasOptionsMenu(true)
