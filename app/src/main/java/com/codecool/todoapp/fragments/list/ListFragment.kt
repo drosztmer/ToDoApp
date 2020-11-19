f0d7caf46663e5e458b20bd96a08f154ee673f2f
Drosztmér István <drosztmer.istvan@gmail.com>
2020-11-19 14:03:19 +0100

    Implement sort functionality

--- app/src/main/java/com/codecool/todoapp/fragments/list/ListFragment.kt
+++ app/src/main/java/com/codecool/todoapp/fragments/list/ListFragment.kt
@@ -95,8 +95,18 @@ class ListFragment : Fragment(), SearchView.OnQueryTextListener {
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        if (item.itemId == R.id.menu_delete_all) {
-            confirmRemoval()
+        when (item.itemId) {
+            R.id.menu_delete_all -> confirmRemoval()
+            R.id.menu_priority_high -> mToDoViewModel.sortByHighPriority.observe(
+                viewLifecycleOwner,
+                Observer {
+                    listAdapter.setData(it)
+                })
+            R.id.menu_priority_low -> mToDoViewModel.sortByLowPriority.observe(
+                viewLifecycleOwner, Observer {
+                    listAdapter.setData(it)
+                }
+            )
         }
         return super.onOptionsItemSelected(item)
     }
